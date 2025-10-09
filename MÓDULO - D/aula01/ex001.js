40d24aca3010fdf73c13d166aaf428aef9637f4c
LuisGomesHCJ <luisgomes.trab@gmail.com>
2025-10-09 16:46:32 -0300

    Praticando

---
+++ MÓDULO - D/aula01/ex001.js
@@ -0,0 +1,10 @@
+// CONDIÇÃO SIMPLES ( SEM else )
+
+var vel = 60.6
+console.log(`A velocidade do seu automóvel é de ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida!!')
+
+}
+console.log('Dirija sempre com cinto de segurança!!')
---
+++ MÓDULO - D/aula01/ex002.js
@@ -0,0 +1,12 @@
+var pais = 'BR'
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'BR') {
+
+    console.log('Você é Brasileiro')
+
+} else {
+
+    console.log('Você é estranjeiro... Não é bem vindo!')
+
+}
