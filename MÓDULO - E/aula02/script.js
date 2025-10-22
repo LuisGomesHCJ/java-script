b9669c9a7334e084a988ef4117bff20329cc976a
LuisGomesHCJ <luisgomes.trab@gmail.com>
2025-10-22 01:14:58 -0300

    Update scripts e local da pasta

--- MÓDULO - E/aula02/script.js
+++ MÓDULO - E/aula02/script.js
@@ -7,25 +7,25 @@ function contar() {
 
 
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { // Se o usuário não preencher os dados.
         resultado.innerHTML = (`Você não preencheu todos os dados, como quer contar assim???`)
     }
     else {
         resultado.innerHTML = (`Contando: <br>`)
-        let ini = Number(inicio.value)
-        let fm = Number(fim.value)
-        let pass = Number(passo.value)
-        if (pass <= 0 ) {
+        let ini = Number(inicio.value) // Cria o (ini), transfere o valor de (inicio) e transforma esse valor em número (Number)
+        let fm = Number(fim.value)     // Cria o (fm), transfere o valor de (fim) e transforma esse valor em número (Number) 
+        let pass = Number(passo.value) // Cria o (pass), transfere o valor de (passo) e transforma esse valor em número (Number)
+        if (pass <= 0 ) { // se pass for menor ou igual a 0
             alert (`Passo inválido, considerando Passo: 01`)
             pass = 1
         }
 
-        if (ini < fm) { // Contagem crescente
+        if (ini < fm) { // Contagem crescente / se ini for menor que fm
             for (let contador = ini ; contador <= fm ; contador += pass) {
                 resultado.innerHTML += (` ${contador} \u{1F449}`)
             }
         }
-        else { // Contagem decrescente 
+        else { // Contagem decrescente / se ini for maior que fm
             for (let contador = ini ; contador >= fm ; contador -= pass ) {
                 resultado.innerHTML += (` ${contador} \u{1F449}`)
             }
